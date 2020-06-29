9292675c4cfb216a2853368702cf6fa345515f89
Sergi Casbas <sergi@casbas.cat>
2020-06-29 16:28:02 +0200

    Added some new unit tests

--- unit-test.sh
+++ unit-test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-
-cat unit-test/body.txt | ./simplelist.py --sender=sergi.casbas@rstic.es --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
+clear
+echo "Empty" | ./simplelist.py --verbose=6 --sender=sergi.casbas@rstic.es --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
+echo "Empty" | ./simplelist.py --verbose=6 --sender=sergi.casbas@gft.com --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
+echo "Empty" | ./simplelist.py --verbose=6 --sender=sergi.casbas@gft.com --recipient=unsubscribe-milista@lists.rstic.es --local=unsubscribe-milista --domain=lists.rstic.es --mailbox=Inbox
+echo "Empty" | ./simplelist.py --verbose=6 --sender=sergi@casbas.cat --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
+cat unit-test/body.txt | ./simplelist.py --verbose=6 --sender=sergi.casbas@rstic.es --recipient=milista@lists.rstic.es --local=milista --domain=lists.rstic.es --mailbox=Inbox
 
