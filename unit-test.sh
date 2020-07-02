10b70444e9d3f08e7ece634a4f37aeed0f159463
Sergi Casbas <sergi@casbas.cat>
2020-07-02 08:48:05 +0200

    Unit tests simplification.

--- unit-test.sh
+++ unit-test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 clear
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
-echo "Empty" | ./simplelist.py --verbose=6 --sender=sergi.casbas@gft.com --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
-echo "Empty" | ./simplelist.py --verbose=6 --sender=sergi.casbas@gft.com --recipient=unsubscribe-milista@lists.rstic.es --local=unsubscribe-milista --domain=lists.rstic.es --mailbox=Inbox
-echo "Empty" | ./simplelist.py --verbose=7 --sender=sergi@casbas.cat --recipient=subscribe-milista@lists.rstic.es --local=subscribe-milista --domain=lists.rstic.es --mailbox=Inbox
-cat unit-test/body.txt | ./simplelist.py --verbose=6 --sender=sergi.casbas@rstic.es --recipient=milista@lists.rstic.es --local=milista --domain=lists.rstic.es --mailbox=Inbox
+echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=help-me               --domain=lists.rstic.es 
+echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=subscribe-unit-test   --domain=lists.rstic.es
+echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=unsubscribe-unit-test --domain=lists.rstic.es
+echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=subscribe-unit-test   --domain=lists.rstic.es
+echo "Body " | ./simplelist.py --sender=sergi.casbas@rstic.es --local=unit-test            --domain=lists.rstic.es
 
