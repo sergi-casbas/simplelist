0478688b4e10a2a790881649dcce6762c76a64e8
Sergi Casbas <sergi@casbas.cat>
2020-09-09 17:19:57 +0200

    Refactor dummy adresses.

--- unit-test.sh
+++ unit-test.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
+
+# Dummy smtp server:
+# python -m smtpd -c DebuggingServer -n localhost:2525
+
 set -e
 clear
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=subscribe             --domain=lists.rstic.es
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=help                  --domain=lists.rstic.es 
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=help-me               --domain=lists.rstic.es 
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=subscribe-unit-test   --domain=lists.rstic.es
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=unsubscribe-unit-test --domain=lists.rstic.es
-echo "Empty" | ./simplelist.py --sender=sergi.casbas@rstic.es --local=subscribe-unit-test   --domain=lists.rstic.es
-echo "Body " | ./simplelist.py --sender=sergi.casbas@rstic.es --local=unit-test             --domain=lists.rstic.es
+echo "Empty" | ./simplelist.py --sender=me@dummy.domain --local=subscribe             --domain=lists.dummy.domain
+echo "Empty" | ./simplelist.py --sender=me@dummy.domain --local=help                  --domain=lists.dummy.domain 
+echo "Empty" | ./simplelist.py --sender=me@dummy.domain --local=help-me               --domain=lists.dummy.domain 
+echo "Empty" | ./simplelist.py --sender=me@dummy.domain --local=subscribe-unit-test   --domain=lists.dummy.domain
+echo "Empty" | ./simplelist.py --sender=me@dummy.domain --local=unsubscribe-unit-test --domain=lists.dummy.domain
+echo "Empty" | ./simplelist.py --sender=me@dummy.domain --local=subscribe-unit-test   --domain=lists.dummy.domain
+echo "Body " | ./simplelist.py --sender=me@dummy.domain --local=unit-test             --domain=lists.dummy.domain
 
-cat unit-test/body-auto-generated-no.eml |    ./simplelist.py --sender=sergi.casbas@rstic.es --local=unit-test --domain=lists.rstic.es
-cat unit-test/body-auto-generated.eml |    ./simplelist.py --sender=sergi.casbas@rstic.es --local=unit-test --domain=lists.rstic.es
-cat unit-test/body-auto-submitted.eml |    ./simplelist.py --sender=sergi.casbas@rstic.es --local=unit-test --domain=lists.rstic.es
+echo "Empty" | ./simplelist.py --sender=alter.ego@dummy.domain --local=subscribe-unit-test             --domain=lists.dummy.domain
+
+cat unit-test/body-auto-generated-no.eml |    ./simplelist.py --sender=me@dummy.domain --local=unit-test --domain=lists.dummy.domain
+cat unit-test/body-auto-generated.eml |    ./simplelist.py --sender=me@dummy.domain --local=unit-test --domain=lists.dummy.domain
+cat unit-test/body-auto-submitted.eml |    ./simplelist.py --sender=me@dummy.domain --local=unit-test --domain=lists.dummy.domain
 
