94ad84af787407b01f0f43faa82905b06cb8035a
Sergi Casbas <sergi@casbas.cat>
2020-09-16 15:40:04 +0200

    Refactor to use the right SQL comment statement.

--- create-tables.sql
+++ create-tables.sql
@@ -1,17 +1,18 @@
-## File with create table statements to initialize database.
+-- File with create table statements to initialize database.
 
-## SQLITE3
-# subscripions table.
+-- SQLITE3
+
+-- subscripions table.
 CREATE TABLE `subscriptions` 
 ( `maillist` TEXT, `subscriptor` INTEGER, PRIMARY KEY(`maillist`,`subscriptor`) );
 CREATE INDEX `subscriptions_maillists` ON `subscriptions` ( `maillist` );
 
-# private lists table.
+-- private lists table.
 CREATE TABLE `private`
 ( `maillist` TEXT, `administrator` INTEGER, PRIMARY KEY(`maillist`,`administrator`) );
 CREATE INDEX `private_maillist` ON `private` ( `maillist` );
 
-# authorizations table.
+-- authorizations table.
 CREATE TABLE `authorization`
 ( `authorization` TEXT, `maillist` TEXT, `subscriptor` INTEGER, PRIMARY KEY(`authorization`) );
 
